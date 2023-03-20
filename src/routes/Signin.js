a0db02b3fb5b8810f2f00e882b0454085773cb05
anastafish <anastafish24@gmail.com>
2023-03-21 00:48:58 +0300

    changed the loader animation shape

--- src/routes/Signin.js
+++ src/routes/Signin.js
@@ -3,7 +3,7 @@ import { UserContext } from '..';
 import { Link } from "react-router-dom"
 import {  TextField, Button, Alert } from '@mui/material';
 import bg from '../images/bg.jpg'
-import PropagateLoader from "react-spinners/PropagateLoader";
+import ClipLoader from "react-spinners/ClipLoader";
 
 
 function Signin() {
@@ -49,41 +49,43 @@ function Signin() {
   }
 
   return (
-    <div
-    style={{ backgroundImage:`url(${bg})`, backgroundRepeat:"no-repeat",
-     backgroundSize:"cover"}}
-     className='flex flex-col items-center justify-center w-[100vw] h-[100vh] gap-5'>
-      {loading && <PropagateLoader 
-        size={40}
-        color='rgba(256, 256, 256, 1)'
-        aria-label="Loading Spinner"
-        data-testid="loader"
-        />}
-      {error && <Alert
-             severity="warning"
-              className='absolute top-5'
-              >{error}</Alert> }
-        {!loading && <div className='bg-white bg-opacity-95 rounded-lg p-5
-                        flex flex-col items justify-center gap-3
-        '>
-          <h1 className='text-[33px] self-start font-extrabold'>Sign Up</h1>
-          <div className='flex flex-col gap-2 w-[100%]'>
-              <TextField name='name' autoFocus value={userInfo.name} onChange={handleChange} fullWidth={true} label="Full Name"/>
-              <TextField name='email' value={userInfo.email} onChange={handleChange} label="Email Address"/>
-              <TextField name='password' value={userInfo.password} onChange={handleChange} label="Password" type='password'/>
-              <TextField name='confirmPassword' value={userInfo.confirmPassword} onChange={handleChange} label="Confirm Password" type='password'/>
-          </div>
-          <Button color='warning' variant="contained" onClick={signUp}>Signup now</Button>
-          <Link to={'/login'}>
-              <h4 className='text-blue-400 text-center'>Already have an account?</h4>
-          </Link>
-          <a className='absolute bottom-0 text-white'
-          target='_blank'
-          rel="noreferrer"
-           href="https://www.freepik.com/free-photo/two-elderly-people-train-station_7629829.htm#query=train%20background&position=3&from_view=keyword&track=ais">
-            Image by wirestock on Freepik</a> 
-        </div>}
-        
+    <div className='flex flex-col items-center justify-center w-[100vw] h-[100vh]'>
+      {loading && <ClipLoader
+          size={125}
+          height='20'
+          color='#94A9A8 '
+          aria-label="Loading Spinner"
+          data-testid="loader"
+          />}
+      {!loading && <div
+      style={{ backgroundImage:`url(${bg})`, backgroundRepeat:"no-repeat",
+       backgroundSize:"cover"}}
+       className='flex flex-col items-center justify-center w-[100vw] h-[100vh] gap-5'>        
+        {error && <Alert
+               severity="warning"
+                className='absolute top-5'
+                >{error}</Alert> }
+           <div className='bg-white bg-opacity-95 rounded-lg p-5
+                          flex flex-col items justify-center gap-3
+          '>
+            <h1 className='text-[33px] self-start font-extrabold'>Sign Up</h1>
+            <div className='flex flex-col gap-2 w-[100%]'>
+                <TextField name='name' autoFocus value={userInfo.name} onChange={handleChange} fullWidth={true} label="Full Name"/>
+                <TextField name='email' value={userInfo.email} onChange={handleChange} label="Email Address"/>
+                <TextField name='password' value={userInfo.password} onChange={handleChange} label="Password" type='password'/>
+                <TextField name='confirmPassword' value={userInfo.confirmPassword} onChange={handleChange} label="Confirm Password" type='password'/>
+            </div>
+            <Button color='warning' variant="contained" onClick={signUp}>Signup now</Button>
+            <Link to={'/login'}>
+                <h4 className='text-blue-400 text-center'>Already have an account?</h4>
+            </Link>
+            <a className='absolute bottom-0 text-white'
+            target='_blank'
+            rel="noreferrer"
+             href="https://www.freepik.com/free-photo/two-elderly-people-train-station_7629829.htm#query=train%20background&position=3&from_view=keyword&track=ais">
+              Image by wirestock on Freepik</a>
+          </div>      
+      </div>}
     </div>
 
   )
